f9941c91e1517700f59fd9c82246e68b2b310d50
Aquintoms <210276701+Aquintoms@users.noreply.github.com>
2026-03-31 15:16:15 +0300

    Added a new graph and improved on the overall code justifying some of the approaches

--- Week 3/Linearity_assumptions.R
+++ Week 3/Linearity_assumptions.R
@@ -1,4 +1,5 @@
-libs <- c("tidyverse", "dplyr", "gridExtra", "ggplot2", "ggfortify", "broom", "ggcorrplot", "corrr", "corrplot", "MASS", "datarium", "broom")
+libs <- c("tidyverse", "dplyr", "gridExtra", "ggplot2", "ggfortify", 
+          "broom", "ggcorrplot", "corrr", "corrplot", "MASS", "datarium", "broom")
 
 for (ilib in libs) {
   if (!(ilib %in% installed.packages())) {
@@ -9,7 +10,7 @@ for (ilib in libs) {
 
 theme_set(theme_classic())
 
-df <- read.csv("C:/Users/HP/OneDrive - Strathmore University/STRATH UNI/FIRST YEAR/SEMESTER III/UNITS/LINEAR MODELS/CSVs/FuelConsumption.csv")
+df <- read.csv("C:/Users/HP/OneDrive - Strathmore University/STRATH UNI/FIRST YEAR/SEMESTER III/UNITS/LINEAR MODELS/LINEAR-MODELS/Data/FuelConsumption.csv")
 
 cdf <- df[, c("ENGINESIZE", "CYLINDERS", "FUELCONSUMPTION_COMB", "CO2EMISSIONS")] # selecting relevant columns
 
@@ -19,24 +20,45 @@ names(df)
 
 View(df)
 
+# Running some EDA by checking skewness and trying to spot some outliers
+
+diff(range(cdf$CYLINDERS))
 ggplot(cdf,aes(x = CYLINDERS)) +
   geom_histogram(binwidth = 1, fill = "blue", color = "black") +
   labs(title = "Histogram of Cylinders", x = "Cylinders", y = "Count")
-# The above graph is positively skewed thus violating the assumption of normality
+# The above histogram is positively skewed. Most vehicles had less than 6 cylinders
 
+ggplot(cdf, aes(x = CYLINDERS)) +
+  geom_bar(fill = "blue", color = "black") + 
+  labs(title = "Bar graph showing the no. of Cylinders", x = "Cylinders", y = "Count")
+# From the bar graph, most vehicles seemed to have approximately 4 cylinders.
+
+summary(cdf$ENGINESIZE)
+diff(range(cdf$ENGINESIZE)) # Used this to aid in identifying the bin width
 ggplot(cdf,aes(x = ENGINESIZE)) +
-  geom_histogram(binwidth = 0.5, fill = "red", color = "black") +
+  geom_histogram(binwidth = 0.2, fill = "red", color = "black") +
   labs(title = "Histogram of Engine Size", x = "Engine Size", y = "Count")
-# This graph does not promote assumption of normality
+# This histogram is somewhat positively skewed
 
+diff(range(cdf$FUELCONSUMPTION_COMB))
 ggplot(cdf,aes(x = FUELCONSUMPTION_COMB)) +
-  geom_histogram(binwidth = 1, fill = "green", color = "black") +
+  geom_histogram(binwidth = 0.7, fill = "green", color = "black") +
   labs(title = "Histogram of Fuel Consumption", x = "Fuel Consumption", y = "Count")
-#
+# This histogram is positively skewed.
 
+diff(range(cdf$CO2EMISSIONS))
 ggplot(cdf,aes(x = CO2EMISSIONS)) +
   geom_histogram(binwidth = 10, fill = "purple", color = "black") +
   labs(title = "Histogram of CO2 Emissions", x = "CO2 Emissions", y = "Count")
+# This histogram is slightly normal with a some positive skewness
+
+# Key takeaways
+# 1. Most histograms happened to have a positive skew as there were some outliers seen on the tails
+# 2. diff(range()) helped to identify the bins in the histogram. This was to prevent some random guesses and
+#     be rooted mathematically
+# 3. The cylinder feature had 2 plots, the histogram and the bar graph. The bar graph was more intuitive.
+#     This is because the number if cylinders are discrete and not continuous. A bar graph was more preferred.
+
 
 ### DENSITY PLOT
 ggplot(cdf,aes(x = CYLINDERS)) +
